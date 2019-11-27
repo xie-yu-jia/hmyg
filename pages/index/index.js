b969216d3c6317c1ddf25480b1fb105371785efc
xie-yu-jia <54892013+xie-yu-jia@users.noreply.github.com>
2019-11-27 18:45:10 +0800

    首页轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,20 @@
 //Page Object
 Page({
   data: {
-    
+    // 轮播图图片数组
+    swiperList:[]
   },
   //options(Object)
   onLoad: function(options) {
-    
+    // 获取轮播图图片
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result);
+        this.setData({swiperList: result.data.message})
+      },
+    });
+      
   },
   onReady: function() {
     
