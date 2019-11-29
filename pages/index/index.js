1a477cef701878e2eaba70c60451437949345600
xie-yu-jia <54892013+xie-yu-jia@users.noreply.github.com>
2019-11-29 15:14:04 +0800

    封装request请求+商品分类页引入头部组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,72 +1,64 @@
-//Page Object
+import request from "../../request/request"
+
 Page({
   data: {
     // 轮播图图片数组
-    swiperList:[],
+    swiperList: [],
     // 导航栏数组
-    navs:[],
+    navs: [],
     // 楼层数组
-    floorList:[]
+    floorList: []
   },
   //options(Object)
-  onLoad: function(options) {
+  onLoad: function (options) {
     // 获取轮播图图片
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      success: (result) => {
+    request({url: 'home/swiperdata'})
+      .then(result => {
         // console.log(result);
-        this.setData({swiperList: result.data.message})
-      },
-    });
+        this.setData({ swiperList: result.data.message })
+      });
 
     // 获取导航栏数据
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
-      success: (result) => {
-        //  console.log(result);
-         this.setData({navs: result.data.message})
-      }
-    });
+    request({url: 'home/catitems'})
+      .then(result => {
+        // console.log(result);
+        this.setData({ navs: result.data.message })
+      });
 
     // 获取楼层数据
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
-      success: (result) => {
+    request({url: 'home/floordata'})
+      .then(result => {
         // console.log(result);
-        this.setData({floorList: result.data.message})
-      }
-    });
-      
-      
-      
-  },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
+        this.setData({ floorList: result.data.message })
+      });
 
   },
-  onUnload: function() {
+  onReady: function () {
 
   },
-  onPullDownRefresh: function() {
+  onShow: function () {
 
   },
-  onReachBottom: function() {
+  onHide: function () {
 
   },
-  onShareAppMessage: function() {
+  onUnload: function () {
 
   },
-  onPageScroll: function() {
+  onPullDownRefresh: function () {
+
+  },
+  onReachBottom: function () {
+
+  },
+  onShareAppMessage: function () {
+
+  },
+  onPageScroll: function () {
 
   },
   //item(index,pagePath,text)
-  onTabItemTap:function(item) {
+  onTabItemTap: function (item) {
 
   }
 });
-  
