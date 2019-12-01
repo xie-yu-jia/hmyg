805b26ce2c37f117393674ac0bb8e67d0c0ad3ae
xie-yu-jia <54892013+xie-yu-jia@users.noreply.github.com>
2019-12-01 17:16:52 +0800

    封装统一的加载中效果

--- request/request.js
+++ request/request.js
@@ -1,11 +1,30 @@
+// 定义一个变量  记录同时发送请求的个数
+let ajaxCount = 0;
+
 function axios({url, ...params}) {
+
+    ajaxCount++
+
     const baseURL = "https://api.zbztb.cn/api/public/v1/";
     return new Promise((resolve,reject)=>{
+        // 显示加载中
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        });
+
         wx.request({
             url: baseURL + url,
             ...params,
             success: (result) => {
                 resolve(result)
+            },
+            // 不管成功失败都会执行的函数
+            complete(){
+                ajaxCount--;
+                if(ajaxCount === 0){
+                    wx.hideLoading();
+                }
             }
         });
           
