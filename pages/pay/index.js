0f392f55c8c54c762f13c22afa2bd11432651800
xie-yu-jia <54892013+xie-yu-jia@users.noreply.github.com>
2019-12-04 17:57:16 +0800

    支付页的静态样式布局完成

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,55 @@
-// pages/pay/index.js
+// 识别es7语法文件
+import regeneratorRuntime from '../../lib/runtime/runtime';
+// 封装的三个API
+import { getSetting, chooseAddress, openSetting } from "../../utils/wxAsync";
+// pages/cart/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    // 用户的收货地址
+    address: {},
+    // 购物车数组
+    carts: [],
+    // 商品总价格
+    totalPrice: 0,
+    // 商品总数量
+    nums: 0,
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow() {
+    // 把收货地址设置到data中
+    const address = wx.getStorageSync("address") || {};
 
+    // 获取缓存中的购物车数组数据
+    const carts = wx.getStorageSync("carts") || [];
+
+    this.setData({address, carts})
+
+    // 计算总价格 商品总数量
+    this.countAll(carts)
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 计算总价格 商品总数量
+  countAll(carts){
+    let totalPrice = 0
+    let nums = 0
+    // 如果有一个商品没选中, 全选的状态就是false
+    let allChecked = true
+    carts.forEach(e => {
+      if(e.isChecked){
+        totalPrice += e.goods_price * e.nums;
+        nums += e.nums
+      }else{
+        allChecked = false
+      }
+    });
+    this.setData({totalPrice, nums, allChecked})
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
