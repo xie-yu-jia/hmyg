cc775ecfe3018e572ee2815f2a21985bf398b4df
xie-yu-jia <54892013+xie-yu-jia@users.noreply.github.com>
2019-12-14 19:30:23 +0800

    商品搜索页面_防抖

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,11 +1,35 @@
-// pages/search/index.js
+import request from "../../request/request";
+
+//  定时器的id的默认值
+let timeId = -1;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    list: []
+  },
 
+  // 输入框的输入事件  小米
+  handleInput(e) {
+    /* 
+    
+    1 当用户第一次输入的时候 开始一个定时器    id= 0  1s后执行 
+    2 当用户 在 1s 之内 有了第二次的输入   
+      先执行 clearTimeout(timeId); 
+      也开启了一个新的定时器 id=1 
+    3 重复 步骤2 
+      。。。。。
+    
+    */
+    clearTimeout(timeId);
+    timeId = setTimeout(() => {
+      const value = e.detail.value;
+
+      request({ url: "goods/qsearch", data: { query: value } }).then(res => { this.setData({ list: res.data.message }) })
+    }, 1000);
   },
 
   /**
